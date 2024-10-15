89cae5b404de5c92fd242a4aef6a01de66d4532b
Israel <121396259+YsraellSantos@users.noreply.github.com>
2024-10-15 20:17:42 -0300

    Orietação a objetos
    Metodos
    Atributos
    Crostrutores

---
+++ Estudos/src/orientaçaoObjetos/Carro.java
@@ -0,0 +1,21 @@
+package orientaçaoObjetos;
+
+public class Carro {
+
+// atributos
+	
+	String nome;
+	String marca;
+	int anoCarro;
+	float velocidade;
+	String corCarro;
+	
+	void acelerar(int acelerando) {
+		velocidade+=acelerando;
+	}
+	
+	void freiar(int freiando ) {
+		velocidade -=freiando;
+	}
+		
+}
